dafc616e4a0000ca36558a817e780cabb35ea264
tugcekiziltepe <56649386+tugcekiziltepe@users.noreply.github.com>
2021-12-30 00:51:25 +0300

    Update stitcher.py

--- stitcher.py
+++ stitcher.py
@@ -1,29 +1,34 @@
 import cv2
 import numpy as np
-
+from numpy.core.fromnumeric import shape
+import math
 
 def stitcher(images):
 
-    image1 = cv2.copyMakeBorder(images[0],300,300,600,600, cv2.BORDER_CONSTANT)
+
     image2 = images[1]
     image3 = images[2]
-    (homography_1, key_points_1, key_points_2, mask_1) = transform(images[2], image1)
-    (homography_2, key_points_2, key_points_3, mask_2) = transform(images[1], image1)
+    # Add padding to allow space around the center image to paste other images.
+    image1 = images[0]
     
-    m = np.ones_like(image3, dtype='float32')
-    m1 = np.ones_like(image2, dtype='float32')
+    # Calculate homography transformation for img2 to img1.
+    (M21, pts221, pts121, mask21) = transform(image1,image2)
+    
+    # Transformed image of img1 and img2 using homography.
+    tranformedImage2 = cv2.warpPerspective(image2, M21, (image1.shape[1],image1.shape[0]), dst=image1.copy(),borderMode=cv2.BORDER_CONSTANT)
 
-    out1 = cv2.warpPerspective(image3, homography_1, (image1.shape[1],image1.shape[0]))
-    out2 = cv2.warpPerspective(image2, homography_2, (image1.shape[1],image1.shape[0]))
-    out3 = cv2.warpPerspective(m, homography_1, (image1.shape[1],image1.shape[0]))
-    out4 = cv2.warpPerspective(m1, homography_2, (image1.shape[1],image1.shape[0]))
+    # Calculate homography transformation for img3 to img1.
+    (M31, pts231, pts131, mask31) = transform(image2,image3)
+    tranformedImage3 = cv2.warpPerspective(image3, M31, (image2.shape[1],image2.shape[0]), dst=image2.copy(),borderMode=cv2.BORDER_TRANSPARENT)
 
-    lpb = laplacian_pyramid_blending(out1,image1,out3)
+    # Combine both transformed images
+    # Pass tranformedImage3 as first and tranformedImage2 as second parameter in function because
+    # laplacian function blend by taking first half of first image and second hafl of second image.
+    output_image = laplacian_blending2(tranformedImage3, tranformedImage2, mask31)
+    
+    cv2.imwrite("output.jpg", output_image)
 
-    lpb1 = laplacian_pyramid_blending(out2,lpb,out4)
-    cv2.imwrite('output_homography3_lpb.png',lpb1)
-
-
+    return True
     return True
 
 def laplacian_pyramid_blending(image1, image2, mask, num_levels=10):
@@ -37,15 +42,15 @@ def laplacian_pyramid_blending(image1, image2, mask, num_levels=10):
 
 
     for i in range(num_levels):
-        g_image1 = blur_downsample(g_image1)
-        g_image2 = blur_downsample(g_image2)
-        g_mask = blur_downsample(g_mask)
+        g_image1 = cv2.pyrDown(g_image1)
+        g_image2 = cv2.pyrDown(g_image2)
+        g_mask = cv2.pyrDown(g_mask)
 
         gp_image1.append(np.float32(g_image1))
         gp_image2.append(np.float32(g_image2))
         gp_mask.append(np.float32(g_mask))
 
-# generate Laplacian Pyramids for A,B and masks
+    # generate Laplacian Pyramids for A,B and masks
     lp_image1  = [gp_image1[num_levels-1]] # the bottom of the Lap-pyr holds the last (smallest) Gauss level
     lp_image2  = [gp_image2[num_levels-1]]
     g_mask_lp = [gp_mask[num_levels-1]]
@@ -53,8 +58,8 @@ def laplacian_pyramid_blending(image1, image2, mask, num_levels=10):
     for i in range(num_levels-1,0,-1):
         # Laplacian: subtarct upscaled version of lower level from current level
         # to get the high frequencies
-        l_image1 = np.subtract(gp_image1[i-1], upsample(gp_image1[i], (gp_image1[i-1].shape[1],gp_image1[i-1].shape[0]))  )
-        l_image2 = np.subtract(gp_image2[i-1], upsample(gp_image2[i] ,(gp_image1[i-1].shape[1],gp_image1[i-1].shape[0])) )
+        l_image1 = np.subtract(gp_image1[i-1], cv2.pyrUp(gp_image1[i] ))
+        l_image2 = np.subtract(gp_image2[i-1],  cv2.pyrUp(gp_image2[i]) )
         lp_image1.append(l_image1)
         lp_image2.append(l_image2)
         g_mask_lp.append(gp_mask[i-1]) # also reverse the masks
@@ -71,12 +76,196 @@ def laplacian_pyramid_blending(image1, image2, mask, num_levels=10):
     for i in range(1,num_levels):
         print(i)
         shape = (LS[i].shape[1], LS[i].shape[0])
-        ls_ = upsample(ls_, shape)
+        ls_ = cv2.pyrUp(ls_)
         
         print(ls_.shape, LS[i].shape)
         ls_ = cv2.add(ls_, LS[i])
 
     return ls_
+
+
+def laplacian_blending(image1, image2, mask, num_levels=10):
+    image1_copy = image1.copy()
+    image2_copy = image2.copy()
+    mask_copy = mask.copy()
+
+    gp_image1 = [image1_copy]
+    gp_image2 = [image2_copy]
+    gp_mask = [mask_copy]
+
+    for i in range(num_levels):
+        image1_copy = cv2.pyrDown(image1_copy)
+        gp_image1.append(image1_copy)
+
+        image2_copy = cv2.pyrDown(image2_copy)
+        gp_image2.append(image2_copy)
+
+        mask_copy = cv2.pyrDown(mask_copy)
+        gp_mask.append(mask_copy)
+
+    image1_copy = gp_image1[5]
+    lp_image1 = [image1_copy]
+        
+    image2_copy = gp_image2[5]
+    lp_image2 = [image2_copy]
+        
+    mask_copy = gp_mask[5]
+    lp_mask = [mask_copy] 
+
+    for i in range(num_levels - 1, 0, -1):
+        gp_ex = cv2.pyrUp(gp_image1[i])
+        lap = cv2.subtract(gp_image1[i-1], gp_ex)
+        lp_image1.append(lap)
+
+        gp_ex = cv2.pyrUp(gp_image2[i])
+        lap = cv2.subtract(gp_image2[i-1], gp_ex)
+        lp_image2.append(lap)
+        
+
+        gp_ex = cv2.pyrUp(gp_mask[i])
+        lap = cv2.subtract(gp_mask[i-1], gp_ex)
+        lp_mask.append(lap)
+
+
+    n1_n2_pyramid = []
+    n = 0
+    for image1_lab, image2_lab, mask_lab in zip(lp_image1, lp_image2, lp_mask):
+        n += 1
+        cols, rows, ch = image1_lab.shape
+        laplacian = np.hstack(
+            (image1_lab[:, 0:int(cols/2)], image2_lab[:, int(cols/2):]))
+        n1_n2_pyramid.append(laplacian)
+
+    reconstruct = n1_n2_pyramid[0]
+    for i in range(1, 6):
+        reconstruct = cv2.pyrUp(reconstruct)
+        reconstruct = cv2.add(n1_n2_pyramid[i], reconstruct)
+
+    return reconstruct
+
+def laplacian_blending2(image1, image2, mask, num_levels=10):
+    image1_copy = image1.copy()
+    image2_copy = image2.copy()
+    mask_copy = mask.copy()
+
+    gp_image1 = [image1_copy]
+    gp_image2 = [image2_copy]
+    gp_mask = [mask_copy]
+
+    for i in range(num_levels):
+        image1_copy = cv2.pyrDown(image1_copy)
+        gp_image1.append(image1_copy)
+
+        image2_copy = cv2.pyrDown(image2_copy)
+        gp_image2.append(image2_copy)
+
+        mask_copy = cv2.pyrDown(mask_copy)
+        gp_mask.append(mask_copy)
+
+    image1_copy = gp_image1[5]
+    lp_image1 = [image1_copy]
+        
+    image2_copy = gp_image2[5]
+    lp_image2 = [image2_copy]
+        
+    mask_copy = gp_mask[5]
+    lp_mask = [mask_copy] 
+
+    for i in range(num_levels - 1, 0, -1):
+        size = (gp_image1[i - 1].shape[1], gp_image1[i - 1].shape[0])
+        gp_ex = cv2.pyrUp(gp_image1[i], dstsize=size)
+        lap = cv2.subtract(gp_image1[i-1], gp_ex)
+        lp_image1.append(lap)
+
+        size = (gp_image2[i - 1].shape[1], gp_image2[i - 1].shape[0])
+        gp_ex = cv2.pyrUp(gp_image2[i], dstsize=size)
+        lap = cv2.subtract(gp_image2[i-1], gp_ex)
+        lp_image2.append(lap)
+        
+        size = (gp_mask[i - 1].shape[1], gp_mask[i - 1].shape[0])
+        gp_ex = cv2.pyrUp(gp_mask[i], dstsize=size)
+        lap = cv2.subtract(gp_mask[i-1], gp_ex)
+        lp_mask.append(lap)
+
+    LS = []
+    for image1_lab, image2_lab, mask_lab in zip(lp_image1, lp_image2, lp_mask):
+        print(image1_lab.shape)
+        print(mask_lab.shape)
+        print(image2_lab.shape)
+        ls = image1_lab * mask_lab + image2_lab * (1.0 - mask_lab)
+        LS.append(ls)
+
+    laplacian_top = LS[0]
+    laplacian_lst = [laplacian_top]
+    num_levels = len(LS) - 1
+    for i in range(num_levels):
+        size = (LS[i + 1].shape[1], LS[i + 1].shape[0])
+        laplacian_expanded = cv2.pyrUp(laplacian_top, dstsize=size)
+        laplacian_top = cv2.add(LS[i+1], laplacian_expanded)
+        laplacian_lst.append(laplacian_top)
+
+    return laplacian_lst[num_levels]
+
+def laplacian_blending3(image1, image2, num_levels = 10):
+    image1_copy = image1.copy()
+    image2_copy = image2.copy()
+
+    gp_image1 = [image1_copy]
+    gp_image2 = [image2_copy]
+
+    for i in range(num_levels):
+        image1_copy = cv2.pyrDown(image1_copy)
+        gp_image1.append(image1_copy)
+
+        image2_copy = cv2.pyrDown(image2_copy)
+        gp_image2.append(image2_copy)
+
+
+    image1_copy = gp_image1[5]
+    lp_image1 = [image1_copy]
+        
+    image2_copy = gp_image2[5]
+    lp_image2 = [image2_copy]
+
+
+    for i in range(num_levels - 1, 0, -1):
+        size = (gp_image1[i - 1].shape[1], gp_image1[i - 1].shape[0])
+        gp_ex = cv2.pyrUp(gp_image1[i], dstsize=size)
+        lap = cv2.subtract(gp_image1[i-1], gp_ex)
+        lp_image1.append(lap)
+
+        size = (gp_image2[i - 1].shape[1], gp_image2[i - 1].shape[0])
+        gp_ex = cv2.pyrUp(gp_image2[i], dstsize=size)
+        lap = cv2.subtract(gp_image2[i-1], gp_ex)
+        lp_image2.append(lap)
+
+    LS = []
+    for image1_lab, image2_lab in zip(lp_image1, lp_image2):
+        rows,cols, dims = image1_lab.shape
+        # Merging first and second half of first and second images respectively at each level in pyramid
+        mask1 = np.zeros(image1_lab.shape)
+        mask2 = np.zeros(image2_lab.shape)
+
+        mask1[:, 0:math.floor(cols/ 2)] = 1
+        mask2[:, math.floor(cols / 2):] = 1
+
+        tmp1 = np.multiply(image1_lab, mask1.astype('float32'))
+        tmp2 = np.multiply(image2_lab, mask2.astype('float32'))
+        tmp = np.add(tmp1, tmp2)
+        
+        LS.append(tmp)
+
+
+    laplacian_top = LS[0]
+    laplacian_lst = [laplacian_top]
+    num_levels = len(LS) - 1
+    for i in range(num_levels):
+        size = (LS[i + 1].shape[1], LS[i + 1].shape[0])
+        laplacian_expanded = cv2.pyrUp(laplacian_top, dstsize=size)
+        laplacian_top = cv2.add(LS[i+1], laplacian_expanded)
+        laplacian_lst.append(laplacian_top)
+
+    return laplacian_lst[num_levels]
 
 def transform(image1, image2):
         # convert images to grayscale
@@ -102,7 +291,7 @@ def transform(image1, image2):
         if len(match) == 2 and match[0].distance < match[1].distance * ratio:
             matches.append((match[0].trainIdx, match[0].queryIdx))
 
-    if len(matches) > 10:
+    if len(matches) > 4:
         # construct the two sets of points
         key_points_1 = np.float32([key_points_1[i] for (_, i) in matches])
         key_points_2 = np.float32([key_points_2[i] for (i, _) in matches])
